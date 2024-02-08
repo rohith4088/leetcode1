5dc341873f05e93a892852ed278c398c2ed0f3fd
rohith4088 <2109486@reva.edu.in>
2024-02-08 20:04:40 +0530

    Add concurrency with goroutines.

--- basics.go
+++ basics.go
@@ -1,5 +1,17 @@
-package main 
+package main
+
 import "fmt"
-func main(){
-	fmt.Println("hello world")
+import "time"
+
+func somefunc(num string) {
+	fmt.Println(num)
+}
+func main() {
+	go somefunc("1")
+	go somefunc("2")
+	go somefunc("3") // go routine --> running function concurrently
+	//the results are frandom because all the functions are running asynchornously and the main function is not waiting for them to finish.
+	//time.Sleep(time.Second * 2) // this is a hack to wait for the go routines to finish
+	time.Sleep(time.Second * 2)
+	fmt.Println("hi")
 }
