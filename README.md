00661e9b7a570fbf451df5a27021f2866ff8c6b4
rohith4088 <2109486@reva.edu.in>
2024-02-09 07:42:00 +0530

    Commit message: Refactored code and added generics.

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-the codes do not excute just copy paste the body in the leetcode in python3
+# Project Title
+
+A brief description of what this project does and why it is useful.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Installation
+
+Instructions on how to install and set up the project.
+
+## Usage
+
+Examples and instructions on how to use the project.
+
+## Contributing
+
+Guidelines on how to contribute to the project.
+
+## License
+
+Information about the project's license.
+
--- basics.go
+++ basics.go
@@ -1,17 +1,40 @@
 package main
 
 import "fmt"
-import "time"
 
-func somefunc(num string) {
-	fmt.Println(num)
+//import "time"
+
+// func somefunc(num string) {
+// 	fmt.Println(num)
+// }
+// func main() {
+// 	go somefunc("1")
+// 	go somefunc("2")
+// 	go somefunc("3") // go routine --> running function concurrently
+// 	//the results are frandom because all the functions are running asynchornously and the main function is not waiting for them to finish.
+// 	//time.Sleep(time.Second * 2) // this is a hack to wait for the go routines to finish
+// 	time.Sleep(time.Second * 2)
+// 	fmt.Println("hi")
+// }
+
+// channels in go
+// func main() {
+// 	mychannel := make(chan string) // creating a channel
+// 	go func() { // anonymous function
+// 		mychannel <- "hello"
+// 	}() // this is a go routine that is sending a message to the channel
+// 	msg := <-mychannel // this is a blocking call
+// 	fmt.Println((msg))
+	// this implements the joining point for the forked child process
+
+//}
+
+// Generics in go 
+func Add [t int | float64] (a t , b t) t{
+	return (a + b)
 }
-func main() {
-	go somefunc("1")
-	go somefunc("2")
-	go somefunc("3") // go routine --> running function concurrently
-	//the results are frandom because all the functions are running asynchornously and the main function is not waiting for them to finish.
-	//time.Sleep(time.Second * 2) // this is a hack to wait for the go routines to finish
-	time.Sleep(time.Second * 2)
-	fmt.Println("hi")
+func main(){
+	result := Add(1,2)
+	fmt.Println((result))
+
 }
