273cf0820d48eebca5223af5edbcd39dd562a135
rohith4088 <2109486@reva.edu.in>
2024-02-16 21:40:09 +0530

    Refactor memoryprofiler.py to optimize memory usage

--- memoryprofiler.py
+++ memoryprofiler.py
@@ -1,15 +1,15 @@
-# from memory_profiler import profile,memory_usage
-# #file_log = open("memory_profiler.log","w+")
+from memory_profiler import profile,memory_usage
+#file_log = open("memory_profiler.log","w+")
 
-# @profile#stream = file_log)
-# def myfucn(list_size):
-#     l1 = ['hello'] * list_size
-#     l2 = ['world'] * list_size
-#     del l2
-#     return l1
-# myfucn(1000000)
-# myfucn(1000)
-# myfucn(1000000000)
+@profile#stream = file_log)
+def myfucn(list_size):
+    l1 = ['hello'] * list_size
+    l2 = ['world'] * list_size
+    del l2
+    return l1
+myfucn(1000000)
+myfucn(1000)
+myfucn(1000000000)
 
 
 #mprof run --python main.py
