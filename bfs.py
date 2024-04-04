080447395f4d94fa37c7886b6282ef7a3f935316
rohith4088 <2109486@reva.edu.in>
2024-04-04 22:11:31 +0530

    Add is_isomorphic function and update variable names in bfs.py

--- bfs.py
+++ bfs.py
@@ -1,37 +1,38 @@
 #bfs algorithm using queue fifo
 # bfs is level order algorithm
 #implementing level order traversal
+
+#tree class defination
 from collections import deque
-#Definition for a binary tree node.
 class TreeNode:
-    def __init__(self,val = 0,left = None, right = None):
-        self.val = val
+    def __init__(self,data = 0,left = None,right = None):
+        self.data = data
         self.left = left
         self.right = right
 
-def levelorder(root):
-    if root ==  None:
+def LevelOrder(head):
+    if head is None:
         return []
     q = deque()
-    q.append(root)
-    big_list =[]
+    q.append(head) 
+    big_list = []
     while len(q):
         size = len(q)
         small_list = []
         for _ in range(size):
             node = q.popleft()
-            small_list.append(node.val)
+            small_list.append(node.data)
             if node.left:
                 q.append(node.left)
             if node.right:
                 q.append(node.right)
-        small_list.append(big_list)   
+        big_list.append(small_list)
     return big_list
-    
+
 if __name__ == "__main__":
-    #root = [3,9,20,null,null,15,7]
+    #tree sturcuture
+    #[3,9,20,null,null,15,7]
     head = TreeNode(3)
     head.left = TreeNode(9,None,None)
     head.right = TreeNode(20,TreeNode(15,None,None),TreeNode(7,None,None))
-    print(levelorder(head))
-    
+    print(LevelOrder(head))
