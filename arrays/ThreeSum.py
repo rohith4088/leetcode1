9cddd733fa185e9a64e210f1df4f30b5b411586e
rohith4088 <2109486@reva.edu.in>
2024-04-08 20:10:32 +0530

    Refactor ThreeSum function to use two-pointer approach in ThreeSum.py

--- arrays/ThreeSum.py
+++ arrays/ThreeSum.py
@@ -1,10 +1,36 @@
+#Brute Force Approach
+# def threesum(nums):
+#     res = []
+#     for i in range(len(nums)):   
+#         for j in range(i+1 , len(nums)):
+#             for k in range(j+1,len(nums)):
+#                 if nums[i] + nums[j] + nums[k] == 0:
+#                     res.append([nums[i],nums[j],nums[k]])
+#     return res
+
+# print(threesum([-1,0,1,2,-1,-4]))
+
+#USING TWO POINTER APPROACH
+#not sure how it works
+
 def threesum(nums):
     res = []
-    for i in range(len(nums)):   
-        for j in range(i+1 , len(nums)):
-            for k in range(j+1,len(nums)):
-                if nums[i] + nums[j] + nums[k] == 0:
-                    res.append([nums[i],nums[j],nums[k]])
+    nums.sort()
+    for i , a in enumerate(nums):
+        if i > 0 and nums[i-1] == a:
+            continue
+        l , r = i + 1, len(nums) -1 
+        while  l < r :
+            threesum = a + nums[l] + nums[r]
+            if threesum > 0:
+                r -= 1
+            elif threesum < 0:
+                l += 1
+            else:
+                res.append([a,nums[l],nums[r]])
+                l += 1
+                while nums[l] == nums[l-1] and l < r:
+                    l += 1
     return res
-
 print(threesum([-1,0,1,2,-1,-4]))
+        
---
+++ arrays/ThreeSumClosest.py
@@ -0,0 +1,21 @@
+#i dindt write this
+def threesum(nums, target):
+    nums.sort()
+    closest_sum = float('inf')
+    for i in range(len(nums) - 2):
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+        l, r = i + 1, len(nums) - 1
+        while l < r:
+            current_sum = nums[i] + nums[l] + nums[r]
+            if current_sum == target:
+                return target
+            elif current_sum < target:
+                l += 1
+            else:
+                r -= 1
+            closest_sum = min(closest_sum, abs(current_sum - target))
+    return closest_sum
+
+print(threesum([-1, 2, 1, -4], 1)) # Output: 2
+print(threesum([0, 0, 0], 1)) # Output: 0
