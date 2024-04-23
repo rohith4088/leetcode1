3a167c6c1d7f237e6ef0aed173df020e69efb34e
rohith4088 <2109486@reva.edu.in>
2024-04-23 21:37:41 +0530

    graphs

---
+++ graphs/MinimumHeightTreees.py
@@ -0,0 +1,12 @@
+class TreeNoede:
+    def __init__(self,root = 0,left = None,right = None):
+        self.root = root
+        self.left = left
+        self.right = right
+
+
+if __name__ == "__main__":
+    n = 4 #nodes laballed from 0 to n - 1
+    edges = [[1,0],[1,2],[1,3]]
+
+
