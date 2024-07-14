23a9af7691ce8692f52bb24ecb81a4178a81bb6f
rohith4088 <2109486@reva.edu.in>
2024-07-14 09:58:49 +0530

    added few sql codes

--- PythonConcept/Scheduling.py
+++ PythonConcept/Scheduling.py
@@ -1,6 +1,7 @@
 from schedule import repeat , run_pending , every
 import time
 
+import schedule
 #using a decorator
 # @repeat(every(1).seconds)
 # def job():
@@ -12,11 +13,23 @@ import time
 
 #passing arguments to the schdeuler
 
-@repeat(every(2).seconds,"mars")
-@repeat(every(1).seconds,'earth')
-def JobTwo(planet):
-    print("hello",planet)
+# @repeat(every(2).seconds,"mars")
+# @repeat(every(1).seconds,'earth')
+# def JobTwo(planet):
+#     print("hello",planet)
 
 
-while True:
-    run_pending()
+# while True:
+#     run_pending()
+
+import schedule
+
+def greet(name):
+    print('Hello {}'.format(name))
+
+schedule.every().day.do(greet, 'Andrea').tag('daily-tasks', 'friend')
+schedule.every().hour.do(greet, 'John').tag('hourly-tasks', 'friend')
+schedule.every().hour.do(greet, 'Monica').tag('hourly-tasks', 'customer')
+schedule.every().day.do(greet, 'Derek').tag('daily-tasks', 'guest')
+
+friends = schedule.get_jobs('friend')
---
+++ mysql.session.sql
@@ -0,0 +1,5 @@
+SELECT * FROM customers;
+
+INSERT into customers (CID , CNAME , CPHONE)  VALUES (2 , "POOJA" , "8660939142"),(3 , 'MILO' ,'9342521511');
+
+SELECT * FROM customers;
