d209ff97fdc39d7c8c5a74f7f2377d1642db6200
rohith4088 <2109486@reva.edu.in>
2024-07-24 13:13:43 +0530

    added few more exmpales of exception codes

--- Exception/assertion.py
+++ Exception/assertion.py
@@ -8,18 +8,33 @@ def DataProcessing(data , **kwargs):
     pass
 
 
-print(DataProcessing({"id" : 1 , "names" : "rohith"}))
+print(DataProcessing({"id" : 1 , "name" : "rohith"}))
 
 
 #ALGORITHMIC INPLEMNTATION OF EXCEPTION HANDLING IN PYTHON
 
-def get_value_from_dict(data , key):
-    try:
-        value = data[key]
-    except KeyError:
-        print("the key {key} was not found")
-        value = None
-    return value
+# def get_value_from_dict(data , key):
+#     try:
+#         value = data[key]
+#     except KeyError:
+#         print("the key {key} was not found")
+#         value = None
+#     return value
+
+
+
+#defining your own exceptrion
+
+class InvalidWithdrawal(Exception):
+    def __init__(self , amount , balance):
+        super().__init__("your balance is more than your withdrwarl amount")
+        self.amount = amount
+        self.balance = balance
+
+    def overage(self):
+        self.balance = self.amount
+
+raise InvalidWithdrawal(25 , 24)
 
 
 
