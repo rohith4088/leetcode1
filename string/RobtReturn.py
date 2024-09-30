dc16c56d618c9d0bda7b02b18c3ecc5a218bf77e
Rohith R <rohithr@Mac.lan>
2024-09-30 08:04:41 +0530

    robot return to origin code added

---
+++ string/RobtReturn.py
@@ -0,0 +1,17 @@
+#string manipulation
+
+def robot(moves : str) -> bool:
+    x , y = 0 , 0
+    for move in moves:
+        if move == 'U':
+            y += 1
+        elif move == 'D' :
+            y -= 1
+        elif move == 'R':
+            x += 1
+        elif move == 'L':
+            x -= 1
+    return x == 0 and y == 0
+
+moves = "UDD"
+print(robot(moves))
